7186b9dc219d6de850c668249fe1719a0098e0de
Giaco <g_palma@giacomopalma.com>
2015-07-27 13:36:44 -0700

    created models and db tables

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,30 @@
 class PostsController < ApplicationController
 	def index
+		@content_first = "This is some sample text for our test blog for this ruby shit man oneee";
+		@content_second = "This is some sample text for our test blog for this ruby shit man twooo";
+	end
+	
+	def new
 	
 	end
 	
+	def create
+	
+	end
+	
+	def edit
+	
+	end
+	
+	def update
+	
+	end
+	
+	def show
+	
+	end
+	
+	def destroy
+	
+	end
 end
